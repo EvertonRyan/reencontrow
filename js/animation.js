da7316a5b98f1976703ae19aa4cb4debeb43658b
Everton Ryan dos Santos Mendonça <148289798+EvertonRyan@users.noreply.github.com>
2024-04-25 11:47:40 -0400

    refazendo o site do zero e adicionando animações com a biblioteca scroll reveal

---
+++ js/animation.js
@@ -0,0 +1,83 @@
+window.revelar = ScrollReveal({reset: true});
+
+revelar.reveal('.hero', {
+    distance: '90px',
+    duration: 2000
+})
+
+revelar.reveal('.pricipal-topics-title', {
+    distance: '90px',
+    duration: 2000,
+    delay: 500
+})
+
+revelar.reveal('.card1', {
+    distance: '90px',
+    duration: 2000,
+    delay: 1000
+})
+
+revelar.reveal('.card2', {
+    distance: '90px',
+    duration: 2000,
+    delay: 1500
+})
+
+revelar.reveal('.card3', {
+    distance: '90px',
+    duration: 2000,
+    delay: 2000
+})
+
+revelar.reveal('.card4', {
+    distance: '90px',
+    duration: 2000,
+    delay: 2500
+})
+
+revelar.reveal('.community-text', {
+    distance: '90px',
+    duration: 2000
+})
+
+revelar.reveal('.community-img', {
+    origin: 'right',
+    distance: '90px',
+    duration: 2000,
+    delay: 500
+})
+
+revelar.reveal('.form-title', {
+    distance: '90px',
+    duration: 2000
+})
+
+revelar.reveal('#nome', {
+    distance: '90px',
+    duration: 2000,
+    delay: 500
+})
+
+revelar.reveal('#email', {
+    distance: '90px',
+    duration: 2000,
+    delay: 1000
+})
+
+revelar.reveal('.button', {
+    distance: '90px',
+    duration: 2000,
+    delay: 1500
+})
+
+revelar.reveal('.footer-title', {
+    distance: '90px',
+    duration: 2000
+})
+
+revelar.reveal('.footer-social-media', {
+    origin: 'left',
+    distance: '150px',
+    duration: 2000,
+    delay: 500
+})
